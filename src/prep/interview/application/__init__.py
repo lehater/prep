a52b01fbc4060068550af03aa9dcb1b0c6ac1dbe
aaterekhin <Lehater@mail.ru>
2026-09-16 17:36:36 +0300

    Add interview application package

---
+++ src/prep/interview/application/__init__.py
@@ -0,0 +1,10 @@
+from .ports import StudyQuestion, StudySyncResult, StudySystem
+from .sync_questions import SyncInterviewQuestions, SyncReport
+
+__all__ = [
+    "StudyQuestion",
+    "StudySyncResult",
+    "StudySystem",
+    "SyncInterviewQuestions",
+    "SyncReport",
+]
